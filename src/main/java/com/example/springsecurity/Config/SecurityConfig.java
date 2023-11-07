e7fd4722d5c9e431f4ab80ed20e7f690cf5f67ab
animesh-kumar-chaturvedi <52943258+Chaturvedi-Animesh@users.noreply.github.com>
2023-11-07 13:43:32 +0530

    Spring security DB
    User details stored in db with encrypted password

--- src/main/java/com/example/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/Config/SecurityConfig.java
@@ -2,22 +2,17 @@ package com.example.springsecurity.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.example.springsecurity.Service.UserServiceImpl;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-// TODO auth from db
 // TODO role based auth
 //TODO JWT
 @EnableWebSecurity
@@ -30,11 +25,6 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder encoder;
 
-    // @Bean
-    // public AuthenticationManager dAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-    //     auth.userDetailsService(customUserService);
-    //     return auth.build();
-    // }
 
 
     @Bean
